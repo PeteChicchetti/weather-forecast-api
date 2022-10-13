1180910b3d2d17ef45f2fbd065e22d53d7b1e483
PeteChicchetti <pj0524@gmail.com>
2022-10-13 16:47:31 -0400

    added function to get weather by lat and lon from geo api

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,6 +5,20 @@ var todayEl = document.getElementById('today');
 var historyEl = document.getElementById('history');
 var APIkey = 'd07d922262517dc70fc0e185f07feddb'
 
+// Function to retrieve weather based on geo coordinates from location in search
+var getWeather = function(lat, lon) {
+  console.log(lat, lon);
+    //Fetch using the api url, .then that return the response as json, .then that calls renderItems(city, data)
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      //getFiveDayForecast();
+    });
+
+}
+
+// Function to get response from geo and plug in city from search to receive lat and lon on data return
 var getCityData = function (city) {
   fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + APIkey)
   .then((response) => {
@@ -18,6 +32,7 @@ var getCityData = function (city) {
     var lat = data[0].lat;
     var lon = data[0].lon;
     console.log(data)
+    getWeather(lat, lon);
   });
 }
 
