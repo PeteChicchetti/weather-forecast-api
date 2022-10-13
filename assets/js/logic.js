40660d3c295dc941d72aad13a5b30d027cf0726b
PeteChicchetti <pj0524@gmail.com>
2022-10-13 15:32:31 -0400

    tested geo api response

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,31 @@
 //Global Variables
 var searchEl = document.getElementById('searchBox');
-var searchEl = document.getElementById('sbtn');
+var sbtnEl = document.getElementById('sbtn');
 var todayEl = document.getElementById('today');
 var historyEl = document.getElementById('history');
 
-var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=a923a2700d72c12cf89f294fb7005300';
 
+//Function to get search results from lat and lon
+function searchRequest() {
+  var current = searchEl.value.trim();
+  var geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${current}&limit=1&appid=d07d922262517dc70fc0e185f07feddb`;
+
+
+  //Fetch for geo api
+  fetch(geoUrl)
+    //If server is communicating then receive response else load text error to page
+    .then (function (response) {
+      console.log(response)
+      if (response.ok) {
+        return response.json();
+      } else {
+        todayEl.textContent = 'Error loading results'
+        return;
+      }
+    })
+    
+
+
+}
+
+sbtnEl.addEventListener('click', searchRequest);
